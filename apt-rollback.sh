6d5ef2d1d8c4507728aba95bb95756a1cae40f6c
fabio.dellaria <fabio.dellaria@gmail.com>
2020-03-19 11:44:48 +0100

    First release

---
+++ apt-rollback.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Ubuntu apt-RollBack Script
+# By Fabio Dell'Aria - fabio.dellaria@gmail.com - Mar 2020
+
+# Check if the current user is "root" otherwise restart the script with "sudo"...
+[ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
+
+# Instruct the Script to Abort for any command error...
+set -e
+
+echo "Ubuntu apt-RollBack Script - ver. 0.3"
+echo "-------------------------------------"
+
+# Main Variables...
+INSTALLED_PACKAGES=$(cat /var/log/apt/history.log | grep -A4 "Start-Date:" | tail -5 | grep "Install: " | cut -d" " -f2- | sed "s/[(][^)]*[)]//g" | sed "s/ ,//g")
+REMOVED_PACKAGES=$(cat /var/log/apt/history.log | grep -A4 "Start-Date:" | tail -5 | grep -e "Purge: " -e "Remove: " | cut -d" " -f2- | sed "s/[(][^)]*[)]//g" | sed "s/ ,//g")
+
+# Remove last Installed Packages...
+apt purge $INSTALLED_PACKAGES
+
+# Install last Removed Packages...
+apt install $REMOVED_PACKAGES
