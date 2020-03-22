b5e09669b9c7bd5cb4ca2fe208f52e11a67d6d22
fabio.dellaria <fabio.dellaria@gmail.com>
2020-03-22 11:46:27 +0100

    Added the '--last' parameter/command + more checks

--- apt-rollback.sh
+++ apt-rollback.sh
@@ -8,17 +8,36 @@
 # Instruct the Script to Abort for any command error...
 set -e
 
-echo "Ubuntu apt-RollBack Script - ver. 0.4.0"
-echo "---------------------------------------"
 
 # Main Variables...
-
 LOG_FILE=/var/log/apt/history.log
+
+
+# Main Functions...
+function reverse_last_command ()
+{
+  INSTALLED_PACKAGES=$(grep -A4 "Start-Date:" $LOG_FILE | tail -5 | grep "Install: " | cut -d" " -f2- | sed "s/[(][^)]*[)]//g" | sed "s/ ,//g" | sed 's/ *$//g')
+  REMOVED_PACKAGES=$(grep -A4 "Start-Date:" $LOG_FILE | tail -5 | grep -e "Purge: " -e "Remove: " | cut -d" " -f2- | sed "s/[(][^)]*[)]//g" | sed "s/ ,//g" | sed 's/ *$//g')
+  UPGRADED_PACKAGES=$(grep -A4 "Start-Date:" $LOG_FILE | tail -5 | grep "Upgrade: " | cut -d" " -f2- | sed "s/[(][^)]*[)]//g" | sed "s/ ,//g" | sed 's/ *$//g')
+}
+
+function usage_message ()
+{
+  echo "Usage: apt-rollback [--last] [--remove package-name] [--install package-name]";
+}
+
+# Main Code...
+echo "Ubuntu apt-RollBack Script - ver. 0.4.5"
+echo "---------------------------------------"
 
 if [ ! -f "$LOG_FILE" ]; then
   echo "The APT log file '$LOG_FILE' doesn't exist."
   echo
   exit 1
+fi
+
+if [ "$1" == "--last" ]; then
+  reverse_last_command;
 fi
 
 if [ "$1" == "--remove" ]; then
@@ -31,19 +50,21 @@ fi
 
 if [ $# -eq 0 ]; then
   echo "No arguments supplied.";
+  usage_message;
   while true; do
     read -r -p "Do you wish to reverse the last APT command (y/n)? " -e -i"n" Answer
     case $Answer in
         [y] )
-          INSTALLED_PACKAGES=$(grep -A4 "Start-Date:" $LOG_FILE | tail -5 | grep "Install: " | cut -d" " -f2- | sed "s/[(][^)]*[)]//g" | sed "s/ ,//g" | sed 's/ *$//g')
-          REMOVED_PACKAGES=$(grep -A4 "Start-Date:" $LOG_FILE | tail -5 | grep -e "Purge: " -e "Remove: " | cut -d" " -f2- | sed "s/[(][^)]*[)]//g" | sed "s/ ,//g" | sed 's/ *$//g')
-          UPGRADED_PACKAGES=$(grep -A4 "Start-Date:" $LOG_FILE | tail -5 | grep "Upgrade: " | cut -d" " -f2- | sed "s/[(][^)]*[)]//g" | sed "s/ ,//g" | sed 's/ *$//g')
+          reverse_last_command;
           break;;
         [n] ) 
           break;;
         * ) echo "Please answer [y]es or [n]o.";;
     esac
   done
+else
+  echo "'$1' is a wrong parameter";
+  usage_message;
 fi
 
 if  [ -n "$INSTALLED_PACKAGES" ]; then
